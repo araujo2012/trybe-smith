071071d2a04cbb123c9a19268cbc376c671969b1
araujo2012 <araujo2012@hotmail.com>
2022-09-13 19:42:36 -0300

    fix: default db name

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -8,7 +8,7 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE || 'Trybesmith_db',
+  database: process.env.MYSQL_DATABASE || 'Trybesmith',
 });
 
 export default connection;
