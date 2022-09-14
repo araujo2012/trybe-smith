368e2787ff78eddf22a3ccd88f09113af254f7f1
araujo2012 <araujo2012@hotmail.com>
2022-09-14 13:06:46 -0300

    fix: connection, remove specific database

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -8,7 +8,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'db',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'Trybesmith',
 });
 
 export default connection;
